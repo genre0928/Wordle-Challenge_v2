b1c3ab31890605ee8d34a5644b48d5ff51921d41
genre0928 <87413846+genre0928@users.noreply.github.com>
2025-01-14 16:21:20 +0900

    서버 동기화 및 남은 횟수 차감
    - 서버와 동기화하여 정답 불러오기
    - 남은 횟수 차감

--- JS/typingKeyboard.js
+++ JS/typingKeyboard.js
@@ -1,7 +1,7 @@
 let row = 0;
 let attempt = 0;
 let challengeTime = 5;
-const answer = "OLIVE";
+// const answer = "OLIVE";
 
 function typingKeyboard() {
   const handleKeydown = (e) => {
@@ -11,11 +11,15 @@ function typingKeyboard() {
     } else if (e.type === "click") {
       key = e.target.dataset.key;
     }
-    const checkAnswer = () => {
+    const checkAnswer = async () => {
       // 반복문으로 하나씩 돌려서 정답이랑 대조한 뒤 클래스값 추가하는걸로
       // 만약 모두 맞으면 정답 스택 쌓기
       // 반복문 종료 후에 스택 갯수보고 정답으로 할 지 row, attempt 수 조정할 지 판단하도록
       // for문 조건 2, 3번째 속성 반대로함 병신
+      const res = await fetch("/answer");
+      const resJson = await res.json();
+      const answer = await resJson.answer;
+
       for (i = 0; i < 5; i++) {
         const alpha = document.querySelector(
           `.main__game-main__answer-boxes__box[data-index='${row}${i}']`
@@ -43,6 +47,9 @@ function typingKeyboard() {
       } else {
         row++;
         attempt = 0;
+        document.querySelector(
+          ".main__game-info__chance-time"
+        ).innerText = `남은 횟수 : ${--challengeTime}회`;
       }
     };
 
@@ -74,3 +81,8 @@ function typingKeyboard() {
 }
 
 typingKeyboard();
+window.onload = () => {
+  document.querySelector(
+    ".main__game-info__chance-time"
+  ).innerText = `남은 횟수 : ${challengeTime}회`;
+};
